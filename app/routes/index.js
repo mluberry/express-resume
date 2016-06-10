8af5a0df5709bc942991b3072df87a637be1098d
Matthieu Luberry <mluberry@kernix.com>
2016-06-10 15:37:16 +0200

    Update app for React handling

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,21 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-
-router.get('/wo', function(req, res, next) {
-  res.render('wo', { title: 'Wo' });
-});
-
-router.get('/sk', function(req, res, next) {
-  res.render('index', { title: 'Sk' });
-});
-
-router.get('/ed', function(req, res, next) {
-  res.render('index', { title: 'Ed' });
-});
-
-router.get('/abm', function(req, res, next) {
-  res.render('index', { title: 'Abm' });
-});
-
 module.exports = router;
